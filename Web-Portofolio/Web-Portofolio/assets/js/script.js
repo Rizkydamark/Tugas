0c82ca391ec61cdddde8c8e756ab986a18b7525c
Rizkydamark <141046590+Rizkydamark@users.noreply.github.com>
2024-03-22 22:11:58 +0700

    Delete Web-Portofolio/Web-Portofolio directory

--- Web-Portofolio/Web-Portofolio/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-let typed = new Typed(".teks", {
-  strings: ["Sedang Belajar Fullstack Web Development", "Developer Pemula"],
-  typeSpeed: 60,
-  backSpeed: 60,
-  backDelay: 1000,
-  loop: true,
-});
